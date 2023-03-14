b15048179b682da8364e60c0860ce29fe41f48ea
Brian Romaniuk <brirom@telus.net>
2023-03-14 09:02:26 -0700

    Commit lesson 9

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import homePageView, aboutPageView, brianPageView, results, homePost
+from .views import homePageView, aboutPageView, brianPageView, results, homePost, todos
 
 urlpatterns = [
     path('', homePageView, name='home'),
     path('about/', aboutPageView, name='about'),
     path('brian/', brianPageView, name='brian'),
     path('homePost/', homePost, name='homePost'),
-path('results/<int:choice>/<str:gmat>/', results, name='results'),
+    path('results/<int:choice>/<str:gmat>/', results, name='results'),
+    path('todos', todos, name='todos')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.http import Http404
 from django.urls import reverse
 from django.views.generic import TemplateView
-
+from pages.models import Item, ToDoList
 def homePageView(request):
     # return request object and specify page.
     return render(request, 'home.html', {
@@ -73,3 +73,10 @@ def results(request, choice, gmat):
     return render(request, 'results.html', {'choice': workExperience, 'gmat':gmat, 
                 'prediction':singlePrediction})
 
+def todos(request):
+    print("*** Inside todos()")
+    items = Item.objects
+    itemErrandDetail = items.select_related('todolist')
+    print(itemErrandDetail[0].todolist.name)
+    return render(request, 'ToDoItems.html', 
+                {'ToDoItemDetail': itemErrandDetail})
